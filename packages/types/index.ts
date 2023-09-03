da2f6e9708d5dce65738e114af07b1f2411528e8
Ross Bulat <ross@parity.io>
2023-09-03 17:00:06 +0700

    more types

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -4,6 +4,7 @@ SPDX-License-Identifier: Apache-2.0 */
 export * from "./lib/accounts";
 export * from "./lib/blockstream";
 export * from "./lib/ledger";
+export * from "./lib/orchestrator";
 export * from "./lib/misc";
 export * from "./lib/reporter";
 export * from "./lib/tx";
