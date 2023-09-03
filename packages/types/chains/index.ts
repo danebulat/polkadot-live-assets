ab556798eb88c793e52d1e20001f4ae0276fee2a
Ross Bulat <ross@parity.io>
2023-09-03 15:03:46 +0700

    add chain types

---
+++ packages/types/chains/index.ts
@@ -0,0 +1,5 @@
+// Copyright 2023 @paritytech/polkadot-live authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+// Supported chains as string literals.
+export type ChainID = "Polkadot";
